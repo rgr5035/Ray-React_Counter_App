68aca4168e9042676adfdec471387a534b000e90
Robby Ray <rgr5035@gmail.com>
2021-03-02 11:32:28 -0700

    adding styles to buttons

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 
 export default class Counter extends Component {
-    render() { 
-        return (
-        
-        <div>
-            <h1>Hello World</h1>
-            <button>Increment</button>
-        </div>);
-        
+    state = {
+        count: 1,
+    };
+
+    formatCount() {
+        const { count } = this.state
+        return count === 0 ? 'Zero' : count;
     }
-}
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
+    }
+
+    render() {
+        return (      
+        <div>
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            <button className="btn btn-secondary btn-sm">Increment</button>
+        </div>
+        );   
+    }
+
+    
+};
