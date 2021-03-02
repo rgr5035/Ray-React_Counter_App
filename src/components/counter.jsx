6b4e851633e13ee7a25000d3cf28035f255af184
Robby Ray <rgr5035@gmail.com>
2021-03-02 15:12:21 -0700

    creating multiple components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,29 +5,28 @@ export default class Counter extends Component {
     count: 0,
   };
 
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
-  }
-
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
     classes += this.state.count === 0 ? "warning" : "primary";
     return classes;
   }
 
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+
   handleIncrement = () => {
     this.setState({ count: this.state.count + 1 });
-  }
+  };
 
   handleDecrement = () => {
     this.setState({ count: this.state.count - 1 });
-  }
+  };
 
   render() {
     return (
       <div className="text-center mt-3">
-        <h2>Let's Start Counting!</h2>  
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={this.handleIncrement}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 // import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
-import Counter from "./components/counter";
+import Counters from "./components/counters";
 
-ReactDOM.render(<Counter />, document.getElementById("root"));
+ReactDOM.render(<Counters />, document.getElementById("root"));
