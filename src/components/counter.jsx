a747af6b00416b17d221ff21589e9f6933693f49
Robby Ray <rgr5035@gmail.com>
2021-03-02 12:29:48 -0700

    added decrement button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,29 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Counter extends Component {
-    state = {
-        count: 1,
-    };
+  state = {
+    count: 0,
+  };
 
-    formatCount() {
-        const { count } = this.state
-        return count === 0 ? 'Zero' : count;
-    }
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
 
-    getBadgeClasses() {
-        let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
-        return classes;
-    }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
 
-    render() {
-        return (      
-        <div>
-            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-            <button className="btn btn-secondary btn-sm">Increment</button>
-        </div>
-        );   
-    }
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  }
 
-    
-};
+  handleDecrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  }
+
+  render() {
+    return (
+      <div className="text-center mt-5">
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm m-2"
+        >
+          Increment
+        </button>
+        <button
+          onClick={this.handleDecrement}
+          className="btn btn-secondary btn-sm m-2"
+        >
+          Decrement
+        </button>
+      </div>
+    );
+  }
+}
