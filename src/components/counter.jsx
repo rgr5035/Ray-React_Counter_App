1efdaa1f5f81ab50749ff14c8b111dcdf8f353aa
Robby Ray <rgr5035@gmail.com>
2021-03-04 14:38:11 -0700

    added reset button and refactored code for structure

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,28 +1,20 @@
 import React, { Component } from "react";
 
 export default class Counter extends Component {
-  state = {
-    value: this.props.counter.value,
-  };
+  
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value: count } = this.state;
+    const { value: count } = this.props.counter;
     return count === 0 ? "Zero" : count;
   }
 
-  handleIncrement = () => {
-    this.setState({ value: this.state.value + 1 });
-  };
 
-  handleDecrement = () => {
-    this.setState({ value: this.state.value - 1 });
-  };
 
   render() {
     
@@ -31,13 +23,13 @@ export default class Counter extends Component {
         {this.props.children}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm m-2"
         >
           Increment
         </button>
         <button
-          onClick={this.handleDecrement}
+          onClick={() => this.props.onDecrement(this.props.counter)}
           className="btn btn-secondary btn-sm m-2"
         >
           Decrement
