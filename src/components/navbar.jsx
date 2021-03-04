0d80a9e19d6a17e714fdca34f86b7018165fe84c
Robby Ray <rgr5035@gmail.com>
2021-03-04 15:03:48 -0700

    object destructuring

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Navbar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar <span className="baadge badge-pill badge-secondary">{this.props.totalCounters}</span>
-        </a>
-      </nav>
-    );
-  }
+
+//stateless functional component
+const Navbar = ({totalCounters}) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar <span className="baadge badge-pill badge-secondary">{totalCounters}</span>
+      </a>
+    </nav>
+  );
 }
+
 
 export default Navbar;
